70d16191b254935c0d67ce126c51e7d57c792f5a
153724521 <109783124+153724521@users.noreply.github.com>
2023-06-11 21:03:01 +0800

    Update prompt1.cpp
    窗口阻塞

--- prompt1.cpp
+++ prompt1.cpp
@@ -8,6 +8,8 @@ prompt1::prompt1(QWidget *parent) :
     //提示框初始化
     ui->setupUi(this);
     ui->textEdit->setReadOnly(true); //设置文字不可被编辑
+    setWindowFlags(this->windowFlags() |Qt::Dialog);
+    setWindowModality(Qt::ApplicationModal); //阻塞除当前窗体之外的所有的窗体
 }
 
 prompt1::~prompt1()
